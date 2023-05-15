52e00c1dc787c689a97f9408a23c21642f484459
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-05-15 16:15:36 +0530

    Refactored Hardcoded Values with CONSTANTS

--- src/main/java/com/springboot/BlogApplication/Controller/PostController.java
+++ src/main/java/com/springboot/BlogApplication/Controller/PostController.java
@@ -3,6 +3,7 @@ package com.springboot.BlogApplication.Controller;
 import com.springboot.BlogApplication.DTO.PostDTO;
 import com.springboot.BlogApplication.Payload.PostResponse;
 import com.springboot.BlogApplication.Service.PostService;
+import static com.springboot.BlogApplication.Utils.ApplicationConstants.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -24,10 +25,10 @@ public class PostController {
 
     @GetMapping
     public PostResponse getAllPosts(
-            @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo,
-            @RequestParam(value = "pageSize", defaultValue = "10", required = false) int pageSize,
-            @RequestParam(value = "sortBy", defaultValue = "id", required = false) String sortBy,
-            @RequestParam(value = "sortDir", defaultValue = "asc", required = false) String sortDir
+            @RequestParam(value = "pageNo", defaultValue = DEFAULT_PAGE_NUMBER, required = false) int pageNo,
+            @RequestParam(value = "pageSize", defaultValue = DEFAULT_PAGE_SIZE, required = false) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = DEFAULT_SORT_BY, required = false) String sortBy,
+            @RequestParam(value = "sortDir", defaultValue = DEFAULT_SORT_DIRECTION, required = false) String sortDir
     ){
         return postService.getAllPosts(pageNo, pageSize, sortBy, sortDir);
     }
