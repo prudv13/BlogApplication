839dfdc094b4d04d8d8eb59acbfbabf14561797b
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-05-15 15:53:03 +0530

    Sorting support for Get All Posts REST API

--- src/main/java/com/springboot/BlogApplication/Service/PostServiceImpl.java
+++ src/main/java/com/springboot/BlogApplication/Service/PostServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -51,10 +52,10 @@ public class PostServiceImpl implements PostService{
     }
 
     @Override
-    public PostResponse getAllPosts(int pageNo, int pageSize) {
+    public PostResponse getAllPosts(int pageNo, int pageSize, String sortBy) {
 
         // create Pageable instance
-        Pageable pageable = PageRequest.of(pageNo, pageSize);
+        Pageable pageable = PageRequest.of(pageNo, pageSize, Sort.by(sortBy));
         Page<Post> posts = postRepository.findAll(pageable);
 
         // get content for page object
