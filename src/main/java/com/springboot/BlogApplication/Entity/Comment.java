3077371984619059b88dc2b3457dcbe161c795d2
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-05-16 21:50:47 +0530

    Created Comment JPA Entity

---
+++ src/main/java/com/springboot/BlogApplication/Entity/Comment.java
@@ -0,0 +1,31 @@
+package com.springboot.BlogApplication.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "comment")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "body", nullable = false)
+    private String body;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
