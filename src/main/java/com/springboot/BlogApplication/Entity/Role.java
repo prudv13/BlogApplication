56f8abc98ada825f1a1b0a09c5f0e4569571715c
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-05-20 23:58:56 +0530

    Create JPA Entities - User and Role

---
+++ src/main/java/com/springboot/BlogApplication/Entity/Role.java
@@ -0,0 +1,23 @@
+package com.springboot.BlogApplication.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+}
