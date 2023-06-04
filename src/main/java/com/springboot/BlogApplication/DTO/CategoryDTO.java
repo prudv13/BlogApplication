556b620c3cf3ed4c25583d8b63912330b3ce3c8a
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-04 15:51:01 +0530

    Created CategoryRepository, CategoryDTO

---
+++ src/main/java/com/springboot/BlogApplication/DTO/CategoryDTO.java
@@ -0,0 +1,16 @@
+package com.springboot.BlogApplication.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDTO {
+    private Long id;
+    private String name;
+    private String description;
+}
