3a34fa0090cc484e069878174b43a2f5a91dbcf4
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-07 15:34:03 +0530

    added CommandLineRunner Interface, its run method

--- src/main/java/com/springboot/BlogApplication/BlogApplication.java
+++ src/main/java/com/springboot/BlogApplication/BlogApplication.java
@@ -1,10 +1,14 @@
 package com.springboot.BlogApplication;
 
+import com.springboot.BlogApplication.Entity.Role;
+import com.springboot.BlogApplication.Repository.RoleRepository;
 import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -25,7 +29,10 @@ import org.springframework.context.annotation.Bean;
 				url = "https://github.com/prudv13/BlogApplication"
 		)
 )
-public class BlogApplication {
+public class BlogApplication implements CommandLineRunner {
+
+	@Autowired
+	private RoleRepository roleRepository;
 
 	@Bean
 	public ModelMapper modelMapper(){
@@ -36,4 +43,14 @@ public class BlogApplication {
 		SpringApplication.run(BlogApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
+
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+	}
 }
