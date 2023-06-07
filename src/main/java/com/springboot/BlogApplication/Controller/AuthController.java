de89418bf9dbac7f90644f5a12d57b75b559cbd1
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-07 11:58:39 +0530

    Customized Swagger API documentation for Auth Controller

--- src/main/java/com/springboot/BlogApplication/Controller/AuthController.java
+++ src/main/java/com/springboot/BlogApplication/Controller/AuthController.java
@@ -4,6 +4,9 @@ import com.springboot.BlogApplication.DTO.LoginDTO;
 import com.springboot.BlogApplication.DTO.RegisterDTO;
 import com.springboot.BlogApplication.Payload.JwtAuthResponse;
 import com.springboot.BlogApplication.Service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,12 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/auth")
+@Tag(name="Authentication - Generate JWT Token")
 public class AuthController {
 
     @Autowired
     private AuthService authService;
 
     @PostMapping(value = {"/login", "/signin"})
+    @Operation(summary = "Login/Signin REST API", description = "This API is used to login or signin existing user, generates JWT token for authentication")
+    @ApiResponse(responseCode = "201", description = "Http Status 201 CREATED")
     public ResponseEntity<JwtAuthResponse> login(@RequestBody LoginDTO loginDTO){
         String token = authService.login(loginDTO);
         JwtAuthResponse jwtAuthResponse = new JwtAuthResponse();
@@ -28,6 +34,8 @@ public class AuthController {
     }
 
     @PostMapping(value = {"/register", "/signup"})
+    @Operation(summary = "Register/Signup REST API", description = "This API is used to register/signup new user")
+    @ApiResponse(responseCode = "201", description = "Http Status 201 CREATED")
     public ResponseEntity<String> register(@RequestBody RegisterDTO registerDTO){
         String response = authService.register(registerDTO);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
