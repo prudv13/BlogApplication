86c545d7948ec2ec6192cac3661f1ba8b3b1adf7
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-07 12:22:44 +0530

    Customized Swagger API documentation for Comment Controller

--- src/main/java/com/springboot/BlogApplication/Controller/CommentController.java
+++ src/main/java/com/springboot/BlogApplication/Controller/CommentController.java
@@ -2,6 +2,9 @@ package com.springboot.BlogApplication.Controller;
 
 import com.springboot.BlogApplication.DTO.CommentDTO;
 import com.springboot.BlogApplication.Service.CommentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,12 +15,15 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name="CRUD REST APIs for Comment Controller")
 public class CommentController {
 
     @Autowired
     private CommentService commentService;
 
     @PostMapping("/posts/{postId}/comments")
+    @Operation(summary = "Create Comment REST API", description = "This API is used to save new comments into the database")
+    @ApiResponse(responseCode = "201", description = "Http Status 201 CREATED")
     public ResponseEntity<CommentDTO> createComment(
             @PathVariable(value = "postId") Long postId,
             @Valid @RequestBody CommentDTO commentDTO)
@@ -26,11 +32,15 @@ public class CommentController {
     }
 
     @GetMapping("/posts/{postId}/comments")
+    @Operation(summary = "Get Comments by Post ID REST API", description = "This API is used to fetch a list of comments with Post ID as reference from the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     public List<CommentDTO> getCommentsByPostId(@PathVariable(value = "postId") Long postId){
         return commentService.getCommentsByPostId(postId);
     }
 
     @GetMapping("/posts/{postId}/comments/{commentId}")
+    @Operation(summary = "Get Comments by ID REST API", description = "This API is used to fetch a comment with ID as reference from the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     public ResponseEntity<CommentDTO> getCommentById(
             @PathVariable(value = "postId") Long postId,
             @PathVariable(value = "commentId") Long commentId)
@@ -40,6 +50,8 @@ public class CommentController {
     }
 
     @PutMapping("/posts/{postId}/comments/{commentId}")
+    @Operation(summary = "Update Comment REST API", description = "This API is used to update an existing comment in the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     public ResponseEntity<CommentDTO> updateComment(
             @PathVariable(value = "postId") Long postId,
             @PathVariable(value = "commentId") Long commentId,
@@ -50,6 +62,8 @@ public class CommentController {
     }
 
     @DeleteMapping("/posts/{postId}/comments/{commentId}")
+    @Operation(summary = "Delete Comment REST API", description = "This API is used to delete an existing comment in the database")
+    @ApiResponse(responseCode = "200", description = "Http Status 200 OK")
     public ResponseEntity<String> deleteComment(
             @PathVariable(value = "postId") Long postId,
             @PathVariable(value = "commentId") Long commentId)
