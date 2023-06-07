2230148bbc5ed53c16f4b8b75239a780aff1c145
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-07 12:31:11 +0530

    Customized Swagger Models documentation for CategoryDTO

--- src/main/java/com/springboot/BlogApplication/DTO/CategoryDTO.java
+++ src/main/java/com/springboot/BlogApplication/DTO/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.springboot.BlogApplication.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,8 +10,13 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(description = "CategoryDTO Model Information")
 public class CategoryDTO {
     private Long id;
+
+    @Schema(description = "Post Category name")
     private String name;
+
+    @Schema(description = "Post Category description")
     private String description;
 }
