cf8c1fca5243294bdce93400b0a43cb3f7178119
Prudhvi Raj V <prudv.raj06@gmail.com>
2023-06-07 12:45:00 +0530

    removed unnecessary imports

--- src/main/java/com/springboot/BlogApplication/BlogApplication.java
+++ src/main/java/com/springboot/BlogApplication/BlogApplication.java
@@ -4,7 +4,6 @@ import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
